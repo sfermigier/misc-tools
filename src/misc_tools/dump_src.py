7cf7d46d542d2e91c9f756226c852a23cfc3ae1f
Stefane Fermigier <sf@abilian.com>
2025-04-30 11:17:13 +0200

    up

---
+++ src/misc_tools/dump_src.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+"""
+Usage:
+    python script_name.py <source_path> [--suffix <suffix_string>]
+Example:
+    python script_name.py ./my_folder --suffix .py
+    python script_name.py /var/log
+"""
+
+import argparse
+import sys
+from pathlib import Path
+
+
+def main():
+    """Parses arguments and prints them."""
+
+    parser = argparse.ArgumentParser(
+        description='Dump a source tree to stdout.',
+    )
+
+    parser.add_argument(
+        '--suffix',
+        dest='suffix',
+        metavar='SUFFIX_STRING',
+        type=str,
+        default="",
+        help='Optional file suffix to filter by (e.g., "py").'
+    )
+
+    parser.add_argument(
+        'src',
+        metavar='SOURCE_PATH',
+        type=str,
+        help='The source path to process.'
+    )
+
+    try:
+        args = parser.parse_args()
+    except SystemExit as e:
+        if e.code != 0:
+            print("Error: Invalid arguments provided.")
+            parser.print_help()
+        sys.exit(e.code)
+
+    src_path = Path(args.src)
+    suffix = args.suffix
+
+    if suffix:
+        files = sorted(src_path.rglob(f"*.{suffix}"))
+    else:
+        files = sorted(src_path.rglob("*"))
+
+    for path in files:
+        dump_file(path)
+
+
+def dump_file(path):
+    if not path.is_file():
+        return
+
+    actual_suffix = path.suffix[1:]
+    match actual_suffix:
+        case "py" | "php":
+            print(78 * "#")
+            print(f"# File: {path}")
+            print(78 * "#")
+        case "js" | "ts":
+            print("// " + 70 * "-")
+            print(f"// File: {path}")
+            print("// " + 70 * "-")
+        case "md":
+            print("<!-- " + 70 * "-" + "-->")
+            print(f"<!-- File: {path} -->")
+            print("<!-- " + 70 * "-" + "-->")
+        case "pyc":
+            return
+        case _:
+            print("Unknown file type", file=sys.stderr)
+
+    print()
+    print(path.read_text())
+    print()
+    print()
+
+
+if __name__ == "__main__":
+    main()
